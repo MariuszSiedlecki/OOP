f612d7fc72dab261e7623c3c4f5735a359b2b65b
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-04-10 23:40:20 +0200

    Laptops

---
+++ src/Laptop.java
@@ -0,0 +1,42 @@
+public class Laptop {
+    private String name;
+    private int grade;
+
+    public Laptop() {
+    }
+
+    ;
+
+    public Laptop(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void introduce() {
+        String masage = "Hi I'm " + name + " and ";
+        if (grade < 10) {
+            masage += " I'm very slovly laptop ";
+        } else if (grade >= 10 && grade <= 20) {
+            masage += " I'm quite amchine";
+        } else {
+            masage += " I'm very fast laptop ";
+        }
+        System.out.println(masage);
+    }
+}
---
+++ src/MainLaptop.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainLaptop {
+    public static void main(String[] args) {
+        List<Laptop> laptopList = new ArrayList<>();
+        laptopList.add(new Laptop("Lenovo", 8));
+        laptopList.add(new Laptop("Dell", 15));
+        laptopList.add(new Laptop("Asus", 20));
+        laptopList.add(new Laptop("Aple", 25));
+
+        for (Laptop laptop : laptopList) {
+            laptop.introduce();
+        }
+        for (int i = 0; i < laptopList.size(); i++) {
+            if (laptopList.get(i).getGrade() > 20) {
+                System.out.println(laptopList.get(i).getName()
+                        + " It is the best laptop " + laptopList.get(i).getGrade() + ".");
+            }
+
+        }
+    }
+}
