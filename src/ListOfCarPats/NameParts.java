d5ad16a86afad9563bfc9b8e6c3e8e9189824134
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-04-16 13:56:48 +0200

    splitList

---
+++ src/ListOfCarPats/NameParts.java
@@ -0,0 +1,45 @@
+package ListOfCarPats;
+
+public class NameParts {
+    private String name;
+    private int quantity;
+    private double price;
+
+    public NameParts(String name, int quantity, double price) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getGuqntity() {
+        return quantity;
+    }
+
+    public void setGuqntity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "name:'" + name +
+                        ", quantity:" + quantity +
+                        ", price:" + price;
+    }
+}
