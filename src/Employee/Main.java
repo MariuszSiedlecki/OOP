4f99f58d1d4d3bd8fcea0f7d84f7ea4af4f68faa
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-05-09 11:54:09 +0200

    change1

--- src/Employee/Main.java
+++ src/Employee/Main.java
@@ -1,9 +1,6 @@
 package Employee;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -12,21 +9,7 @@ public class Main {
         employeesInfo.displayEmployees();
 
         }
-//        public void displayRichEmployee(){
-//            List<Integer>integerList = new ArrayList<>();
-//            for (int i = 0; i <employeeList.size() ; i++) {
-//                if(employeeList.get(i).getSalary()>7000){
-//                    integerList.add(employeeList.get(i).getId());
-//                }
-//            }
-//            System.out.println("employee who ern mor than 7K");
-//        }
-//        private void loadEmployee(){
-//            this.employeeList = Arrays.asList(
-//                    new Employee(25000,"direction"),
-//                    new Employee(8500, "manager"),
-//                    new Employee(5000, "employee")
-//            );
+
         }
 
 
